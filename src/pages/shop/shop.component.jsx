a96ab6186c5f33e1c8e09697378f72ff7f49d444
NotNikita <nikitabsuir@gmail.com>
2021-06-08 18:26:45 +0300

    update: new shop, collection preview, item pages

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+import SHOP_DATA from './shop.data.js'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+const ShopPage = () => {
+    const [collections, setCollections] = useState(SHOP_DATA)
+
+    return (
+        <div className='shop-page'>
+            {
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
+                ))
+            }
+        </div>
+    )
+}
+
+export default ShopPage
