e361f8f5b511d67e0e2889b0255c99b80a0b5ccd
NotNikita <nikitabsuir@gmail.com>
2021-08-08 23:40:12 +0300

    update: added cartIcon and cartDropdown comps, implemented cartReducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 
 import userReducer from './user/user.reducer'
+import cartReducer from './cart/cart.reducer'
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
 //This component combines all our else
 //states 2ghether
